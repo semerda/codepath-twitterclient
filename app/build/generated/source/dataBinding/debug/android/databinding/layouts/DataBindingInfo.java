97035dda08aa83bca93937f02706dfc5839d7dfe
Ernest Semerda <ernest@semerda.com>
2016-08-14 21:54:54 -0700

    Cleanup.

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="f742e15f-3f59-48ed-a9a1-6a4f86445740", modulePackage="com.purpleblue.apps.twitterclient", sdkRoot="/Users/ernest/Library/Android/sdk",layoutInfoDir="/Users/ernest/Android/codepath-twitterclient/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=19,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="245b6f4f-a0d1-4461-9e0e-2ac27db53b99", modulePackage="com.purpleblue.apps.twitterclient", sdkRoot="/Users/ernest/Library/Android/sdk",layoutInfoDir="/Users/ernest/Android/codepath-twitterclient/app/build/intermediates/data-binding-info/debug",exportClassListTo="",isLibrary=false,minSdk=19,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
